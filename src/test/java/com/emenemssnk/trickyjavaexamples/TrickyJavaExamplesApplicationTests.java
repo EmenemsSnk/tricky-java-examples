b4df1596c2464701e89ff0813ac91d1418d071b3
Michal Dziedzic <michaldziedzic@MacBook-Air-Michal.local>
2023-03-05 00:00:20 +0100

    Initial commit

---
+++ src/test/java/com/emenemssnk/trickyjavaexamples/TrickyJavaExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emenemssnk.trickyjavaexamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrickyJavaExamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
